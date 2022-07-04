72b8cdfd323a8e1cb13c4c4d36168f8537728384
Baia Asanidze <baia.asanidze.1@btu.edu.ge>
2022-07-05 00:17:20 +0400

    Initial commit

---
+++ app/src/main/java/com/example/scrollview/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.scrollview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputType
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+import com.example.scrollview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAddField.setOnClickListener {
+
+            if (checkIfFieldIsEmpty()) {
+                Toast.makeText(this, getString(R.string.empty_field_toast), Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                val newField = AppCompatEditText(this)
+                val scrollView = binding.mainActivityLayout
+                val layoutParams = LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                if (binding.checkBox.isChecked) {
+                    newField.inputType = InputType.TYPE_CLASS_NUMBER
+                    newField.hint = binding.edtFieldValue.text
+                    scrollView.addView(newField, layoutParams)
+                    fieldClear()
+                } else {
+                    newField.inputType = InputType.TYPE_CLASS_TEXT
+                    newField.hint = binding.edtFieldValue.text
+                    scrollView.addView(newField, layoutParams)
+                    fieldClear()
+                }
+            }
+        }
+    }
+
+    private fun checkIfFieldIsEmpty(): Boolean = with(binding) {
+
+        if (binding.edtFieldValue.text.toString().isEmpty()) {
+            return@with true
+        }
+        return@with false
+    }
+
+    private fun fieldClear() {
+        binding.edtFieldValue.text?.clear()
+    }
+
+
+}
